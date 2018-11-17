d5abd18423fd6f03aed819451727b7dbf6726070
Juliana Mota <38047358+JulianaMota@users.noreply.github.com>
2018-11-17 19:29:50 +0100

    event js clone

--- js/events.js
+++ js/events.js
@@ -1,11 +1,29 @@
 const template = document.querySelector("#template-events").content;
 
 function getJazzMusic(){
-    fetch(baseLink+"music?categories=2").then(res => res.json()).then(showEvents);
+    fetch(baseLink+"music?categories=2&_embed").then(res => res.json()).then(showEvents);
 }
 
 function showEvents(eventsList){
-    console.log(eventsList)
+    console.log(eventsList);
+    eventsList.forEach(event =>{
+        console.log(event);
+        const copy = template.cloneNode(true);
+        copy.querySelector("h3").textContent=event.title.rendered;
+
+        if(event._embedded['wp:featuredmedia']){
+            copy.querySelector("img").src=event._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url;
+        }else {
+            copy.querySelector("img").remove();
+        }
+        copy.querySelector(".date").textContent=event.acf.date;
+        copy.querySelector(".hour").textContent=event.acf.hour;
+        copy.querySelector(".price span").textContent=event.acf.door_price;
+        copy.querySelector("a").href="event-detail.html?eventid="+event.id;
+
+        document.querySelector("section").appendChild(copy);
+        }
+    )
 }
 
 getJazzMusic();
