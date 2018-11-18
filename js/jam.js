64e37c6dd92ad2587a3af12a90e4fcb0ecf824d4
Juliana Mota <38047358+JulianaMota@users.noreply.github.com>
2018-11-18 21:25:41 +0100

    icons jam js

--- js/jam.js
+++ js/jam.js
@@ -17,6 +17,31 @@ function showJamS(jamList){
         copy.querySelector(".music-3").textContent=jamS.acf.musician_3;
         copy.querySelector(".music-4").textContent=jamS.acf.musician_4;
 
+        if(jamS.acf.instrument_1.sizes){
+            copy.querySelector(".instrument-1").src=jamS.acf.instrument_1.sizes.thumbnail;
+        }else{
+            copy.querySelector(".instrument-1").remove();
+        }
+
+        if(jamS.acf.instrument_2.sizes){
+            copy.querySelector(".instrument-2").src=jamS.acf.instrument_2.sizes.thumbnail;
+        }else{
+            copy.querySelector(".instrument-2").remove();
+        }
+
+        if(jamS.acf.instrument_3.sizes){
+            copy.querySelector(".instrument-3").src=jamS.acf.instrument_3.sizes.thumbnail;
+        }else{
+            copy.querySelector(".instrument-3").remove();
+        }
+
+        if(jamS.acf.instrument_4.sizes){
+            copy.querySelector(".instrument-4").src=jamS.acf.instrument_4.sizes.thumbnail;
+        }else{
+            copy.querySelector(".instrument-4").remove();
+        }
+
+
         document.querySelector(".jam-session").appendChild(copy);
     })
 }
